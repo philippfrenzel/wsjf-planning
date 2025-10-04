f64774fc2238a5473589125f82d6d2eddfbcdda5
Julian Schramm <hi@julian.center>
2025-10-04 20:31:31 +0200

    fix import

--- resources/js/pages/auth/confirm-password.tsx
+++ resources/js/pages/auth/confirm-password.tsx
@@ -1,11 +1,11 @@
 import InputError from '@/components/input-error';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
 import AuthLayout from '@/layouts/auth-layout';
 import { store } from '@/routes/password/confirm';
 import { Form, Head } from '@inertiajs/react';
-import { LoaderCircle } from 'lucide-react';
 
 export default function ConfirmPassword() {
     return (
