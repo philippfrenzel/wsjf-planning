381cd41d8bb9d5d41baf3e4cea3ab4301ada8ea9
hug0 <hugolovighi@gmail.com>
2026-01-07 14:13:25 +0100

    Update panel icon based on sidebar state and device type (#195)
    * Update panel icons
    
    * Refactor sidebar toggle button to use isMobile state
    
    * Fix import statement formatting in sidebar.tsx
    
    * Fix JSX syntax for sidebar icons
    
    * Update icons
    
    ---------
    
    Co-authored-by: Wendell Adriel <wendelladriel.ti@gmail.com>

--- resources/js/components/ui/sidebar.tsx
+++ resources/js/components/ui/sidebar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
-import { PanelLeftIcon } from "lucide-react"
+import { PanelLeftCloseIcon, PanelLeftOpenIcon } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -253,7 +253,7 @@ function SidebarTrigger({
   onClick,
   ...props
 }: React.ComponentProps<typeof Button>) {
-  const { toggleSidebar } = useSidebar()
+  const { toggleSidebar, isMobile, state } = useSidebar()
 
   return (
     <Button
@@ -268,7 +268,7 @@ function SidebarTrigger({
       }}
       {...props}
     >
-      <PanelLeftIcon />
+       {isMobile || state === "collapsed" ? <PanelLeftOpenIcon /> : <PanelLeftCloseIcon />}
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
